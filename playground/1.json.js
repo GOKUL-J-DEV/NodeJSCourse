2fc1c03eb7f9f1ed6da32b4c10e3db4a8643c903
gokuljsparkout <your_email@example.com>
2024-03-06 23:50:43 +0530

    PARSE JSON - WRITE AND READ JSON

--- playground/1.json.js
+++ playground/1.json.js
@@ -1,15 +1,6 @@
   const fs = require('fs')
   
-  const book = {
-    title : 'To Kill a mocking bird',
-    author : 'Slim Shady'
-  }
-
-  const bookJSON = JSON.stringify(book)
-  console.log(bookJSON);
-
- const parseData =  JSON.parse(bookJSON);
- console.log(parseData.author);
-
-
- fs.writeFileSync('1-json.json',bookJSON)
+ const dataBuffer = fs.readFileSync('1-json.json');
+ const dataJSON = dataBuffer.toString();
+ const data =JSON.parse(dataJSON)
+ console.log(data);
