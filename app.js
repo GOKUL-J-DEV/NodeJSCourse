115cb8f23815cd5f600a66908a6f06d671f18179
gokuljsparkout <your_email@example.com>
2024-03-06 23:01:06 +0530

    add remove note - yargs

--- app.js
+++ app.js
@@ -2,5 +2,21 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-console.log(process.argv);
-console.log(yargs.argv['title']);
+
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    handler:()=> {
+        console.log('Adding a new note')
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a new note',
+    handler:()=> {
+        console.log('Removing a  note')
+    }
+})
+
+console.log(yargs.argv)
