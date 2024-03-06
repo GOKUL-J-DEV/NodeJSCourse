3b25eff3217de87911dc1d2697129da2ba5500aa
gokuljsparkout <your_email@example.com>
2024-03-06 22:45:52 +0530

    Yargs

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+const command = process.argv
+console.log(command);
 
-console.log(process.argv)
-
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
+if(command === 'add'){
+console.log('adding notes');
+}else if (command === 'remove'){
+    console.log('removing notes')
 }
